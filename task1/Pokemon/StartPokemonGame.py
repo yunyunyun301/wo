0787e4b87363d904e402fa65603872b13ef3b1cf
yunyun <3202789603@qq.com>
2026-03-06 23:24:56 +0800

    添加了新的宝可梦胖丁

--- task1/Pokemon/StartPokemonGame.py
+++ task1/Pokemon/StartPokemonGame.py
@@ -3,4 +3,10 @@ import pokemon
 if __name__ == "__main__":
     pokemon.chooseyourpokemon()
     x = pokemon.startgame()
-    print(f"获胜者是 {x.name}!")
+    if x.is_man:
+        print(f"获胜者是你的 {x.name}!")
+    else :
+        print(f"获胜者是电脑的 {x.name}!")
+    print("游戏结束！输入任意键退出...")
+    input()
+    
